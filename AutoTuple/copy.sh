5140d41140b848d4d1b79f38dc649a9e79822463
Christopher A. George <george@physics.ucsb.edu>
2015-01-11 22:40:10 -0800

    copy script

---
+++ AutoTuple/copy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dataSet=$1
+CMS3tag=$2
+
+myDir=`echo $CMS3tag | cut -c 6-`
+longName=`echo $dataSet | awk -F '/' '{print $2 "_" $3}'`
+shortName=`echo $dataSet | awk -F '/' '{print $2}'`
+
+if [ ! -e /hadoop/cms/store/group/snt/phys14/$longName ]
+then
+  mkdir /hadoop/cms/store/group/snt/phys14/$longName
+  mkdir /hadoop/cms/store/group/snt/phys14/$longName/$myDir
+  mv /hadoop/cms/store/user/$USER/$shortName/crab_$longName/$CMS3tag/merged/*.root /hadoop/cms/store/group/snt/phys14/$myDir/$longName
+fi
