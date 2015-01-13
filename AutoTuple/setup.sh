d4cf844ec3f31c44cb20cb202f9c03b978e992b1
Christopher A. George <george@physics.ucsb.edu>
2015-01-12 16:18:00 -0800

    add rudimentary coping scripts

--- AutoTuple/setup.sh
+++ AutoTuple/setup.sh
@@ -10,8 +10,8 @@ tag=`sed -n '2p' $1`
 export PATH=$PATH:`pwd`
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 export SCRAM_ARCH=slc6_amd64_gcc481
-cmsrel CMSSW_7_2_0
-cd CMSSW_7_2_0/src
+scramv1 p -n CMSSW_7_2_0_2 CMSSW CMSSW_7_2_0
+cd CMSSW_7_2_0_2/src
 cmsenv
 git clone git@github.com:cmstas/NtupleMaker.git CMS3/NtupleMaker
 cd CMS3/NtupleMaker
@@ -36,5 +36,7 @@ cp ../../FindLumisPerJob.sh .
 cp ../../das_client.py . 
 cp ../../web_autoTuple .
 cp ../../crabPic.png .
+cp ../../copy.sh .
+cp ../../numEventsROOT.C .
 python submit_crab_jobs.py $1 $gtag $tag
 . monitor.sh $1 $gtag $tag
