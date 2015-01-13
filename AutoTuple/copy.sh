e21a69aa8777fba000751bd8803780f0435b8311
Christopher A. George <george@physics.ucsb.edu>
2015-01-12 16:14:31 -0800

    bug fix

--- AutoTuple/copy.sh
+++ AutoTuple/copy.sh
@@ -11,5 +11,7 @@ if [ ! -e /hadoop/cms/store/group/snt/phys14/$longName ]
 then
   mkdir /hadoop/cms/store/group/snt/phys14/$longName
   mkdir /hadoop/cms/store/group/snt/phys14/$longName/$myDir
-  mv /hadoop/cms/store/user/$USER/$shortName/crab_$longName/$CMS3tag/merged/*.root /hadoop/cms/store/group/snt/phys14/$myDir/$longName
+  mv /hadoop/cms/store/user/$USER/$shortName/crab_$longName/$CMS3tag/merged/*.root /hadoop/cms/store/group/snt/phys14/$longName/$myDir
 fi
+
+root -b -l -q numEventsROOT.C\(\"$longName/$myDir\"\)
