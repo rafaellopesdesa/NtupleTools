da8ce3780336a60d00129b05b190be85c5b974e4
Christopher A. George <george@physics.ucsb.edu>
2015-02-03 12:53:28 -0800

    automatic copying of output to phys14

--- AutoTuple/copy.sh
+++ AutoTuple/copy.sh
@@ -7,11 +7,15 @@ myDir=`echo $CMS3tag | cut -c 6-`
 longName=`echo $dataSet | awk -F '/' '{print $2 "_" $3}'`
 shortName=`echo $dataSet | awk -F '/' '{print $2}'`
 
-if [ ! -e /hadoop/cms/store/group/snt/phys14/$longName ]
+echo "myDir $myDir" 
+echo "longName $longName"
+echo "shortName $shortName"
+
+if [ ! -e /hadoop/cms/store/group/snt/phys14/$longName/$myDir ]
 then
   mkdir /hadoop/cms/store/group/snt/phys14/$longName
   mkdir /hadoop/cms/store/group/snt/phys14/$longName/$myDir
-  mv /hadoop/cms/store/user/$USER/$shortName/crab_$longName/$CMS3tag/merged/*.root /hadoop/cms/store/group/snt/phys14/$longName/$myDir
+  mv /hadoop/cms/store/user/$USER/$shortName/crab_$longName/$CMS3tag/merged/*.root /hadoop/cms/store/group/snt/phys14/$longName/$myDir/
 fi
 
-root -b -l -q numEventsROOT.C\(\"$longName/$myDir\"\)
+root -b -l -q numEventsROOT.C\(\"/hadoop/cms/store/group/snt/phys14/$longName/$myDir\"\)
