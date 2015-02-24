625e98feba372e2c4a0d8b323d3b4f3a544f0db3
jgran <jgran@physics.ucsb.edu>
2015-02-24 10:38:42 -0800

    example usage

--- dataTuple/GetDatasetFiles.sh
+++ dataTuple/GetDatasetFiles.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+#example usage:
+#./GetDatasetFiles.sh /DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM 
+
 ./das_client.py --query="file dataset= $1" | grep "^/store"
