9e0b69c6d419463c0338b1c0060490101b0ba308
jgran <jgran@physics.ucsb.edu>
2015-02-25 14:44:43 -0800

    remove old file

--- dataTuple/GetDatasetFiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#example usage:
-#./GetDatasetFiles.sh /DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM 
-
-./das_client.py --query="file dataset= $1" | grep "^/store"
