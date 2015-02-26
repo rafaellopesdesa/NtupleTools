a7d047a42b0f084eecdc107c9814a0d4d0450725
jgran <jgran@physics.ucsb.edu>
2015-02-25 16:10:15 -0800

    script to checkout file and update completed list

--- dataTuple/checkFile.sh
+++ dataTuple/checkFile.sh
@@ -1,3 +1,11 @@
 #/bin/bash
 
-root -l "isValidFile.C(\"$1\")"
+root -b -q "isValidFile.C(\"$1\")" > validFileOutput.txt
+
+readarray -t results < validFileOutput.txt
+for i in "${results[@]}"
+do
+  if [ "$i" == "FileIsValid" ]; then
+    echo $1 >> /nfs-7/userdata/dataTuple/completedList.txt
+  fi
+done
