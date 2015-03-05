e4786ec27aeeef4e644ddc7de2676d2a1d010354
jgran <jgran@physics.ucsb.edu>
2015-03-04 16:41:40 -0800

    first update for shipping libCMS3

--- cms3withCondor/condorExecutable.sh
+++ cms3withCondor/condorExecutable.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
+if (( $# != 6 )); then
+    echo "Illegal number of arguments."
+    return 1
+fi
+
 HOME=`pwd`
 
-#This stuff to get it to run
-export CMS_PATH=/cvmfs/cms.cern.ch
-export SCRAM_ARCH=slc6_amd64_gcc481
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-source /cvmfs/cms.cern.ch/slc6_amd64_gcc481/lcg/root/5.34.18/bin/thisroot.sh
-export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH:$PWD:${_CONDOR_SCRATCH_DIR}
-export PATH=$ROOTSYS/bin:$PATH:${_CONDOR_SCRATCH_DIR}
-export PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH
-
-#This to get the file name
 configFile=$1
 number=$2
 filedir=$3
 username=$4
 OUTPUT=$5
+libCMS3=$6
 
 #Tell us where we're running
 echo "host: " 
@@ -32,65 +28,30 @@ export COPYDIR=/hadoop/cms/store/user/$username/condor/${DIRNAME}/${filedir}
 gtag="PHYS14_25_V2::All"
 tag="master"
 
-#Set env vars
-export PATH=$PATH:`pwd`
-export SCRAM_ARCH=slc6_amd64_gcc481
-
-#Checkout CMSSW
-scramv1 p -n CMSSW_7_2_0 CMSSW CMSSW_7_2_0
-cd CMSSW_7_2_0/src
+#untar tarball containing CMS3 libraries and python files
+if [ -e $libCMS3 ]
+then
+  tar -xzvf $libCMS3
+else
+  echo "libCMS3 missing!"
+  return 1
+fi
 
 #Set environment
+export CMS_PATH=/cvmfs/cms.cern.ch
+export SCRAM_ARCH=slc6_amd64_gcc481
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+source /cvmfs/cms.cern.ch/slc6_amd64_gcc481/lcg/root/5.34.18/bin/thisroot.sh
+
+pushd .
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc481/cms/cmssw/CMSSW_7_2_0/src/
 eval `scramv1 runtime -sh`
-
-#Get the NtupleMaker, build it
-git clone https://github.com/cmstas/NtupleMaker.git CMS3/NtupleMaker
-cd CMS3/NtupleMaker
-git checkout $tag
-echo "CMSSW_BASE: $CMSSW_BASE"
-cd CMSSW_7_2_0
-
-#SETUP/PATCHES TO SOURCE
-#mv $CMSSW_BASE/src/* $CMSSW_BASE/bullshit/
-mv $HOME/RecoEgamma.tar $CMSSW_BASE/src/
-cd $CMSSW_BASE/src/
-echo "THIS IS CMSSW_BASE/src ls"
-ls
-tar -xvf RecoEgamma.tar
-scram b -j 20 
-echo "THERE ARE $? ERRORS AFTER SCRAMING"
-echo "THIS IS CMSSW_BASE ls"
-ls
-
-##############
-## MVA JetId #
-##############
-# 
-git clone https://github.com/latinos/UserCode-CMG-CMGTools-External $CMSSW_BASE/src/CMGTools/External
-pushd $CMSSW_BASE/src/CMGTools/External
-git checkout V00-03-01
-rm plugins/PileupJetIdProducer.cc
 popd
 
-########################
-## LCG dictionaries #
-########################
-git clone https://github.com/cmstas/Dictionaries $CMSSW_BASE/src/CMS3/Dictionaries
-########################
-
-# run checkdeps
-printf "\nchecking deps:\n"
-git cms-checkdeps -a
-
-# compile
-scram build -c -j 20
-
-cd $CMSSW_BASE/src
-scram b -j 10
-
-#Move the config file
-cd ..
-cp ../$configFile . 
+#The lib and python directories come from the libCMS3 tarball
+export LD_LIBRARY_PATH=$PWD/lib/slc6_amd64_gcc481:$LD_LIBRARY_PATH
+export PATH=$PWD:$PATH
+export PYTHONPATH=$PWD/python:$PYTHONPATH
 
 #Run it
 cmsRun $configFile
