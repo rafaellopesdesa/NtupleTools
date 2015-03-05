e40893b73714a7eee339fa7008c058119a7f217e
jgran <jgran@physics.ucsb.edu>
2015-03-05 13:19:42 -0800

    remove files from previous run

--- dataTuple/dataTupleLauncher.sh
+++ dataTuple/dataTupleLauncher.sh
@@ -1,5 +1,13 @@
 chmod +x ./*.sh
 chmod +x ./*.py
+if [ -e submitList.txt ]
+then
+  rm submitList.txt
+fi
+if [ -e runningList.txt ]
+then
+  rm runningList.txt
+fi
 crontab -l > mycron
 echo "* * * * * cd ${PWD} && /bin/sh ${PWD}/manager.sh >> ${PWD}/cron_log.log" >> mycron
 crontab mycron
