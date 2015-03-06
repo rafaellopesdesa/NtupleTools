23f79197951adce6714fca128c5824d1886b6408
jgran <jgran@physics.ucsb.edu>
2015-03-05 23:33:27 -0800

    script to make libCMS3 tarball

---
+++ cms3withCondor/make_libCMS3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if (( $# != 1 )); then
+  echo "Illegal number of arguments."
+  echo "Must provide CMS3 tag"
+  exit 1
+else
+  THE_CMS3_TAG=$1
+fi
+
+echo "Checkout and build CMS3:"
+
+curl https://raw.githubusercontent.com/cmstas/NtupleMaker/master/install.sh > install.sh
+
+sed -i "s/CMS3Tag=master/CMS3Tag=${THE_CMS3_TAG}/g" install.sh
+
+DIR=$PWD
+
+source install.sh
+cd $CMSSW_BASE
+echo "Making the tarball"
+tar -chzvf lib_$THE_CMS3_TAG.tar.gz lib/ python/
+
+mv lib_$THE_CMS3_TAG.tar.gz $DIR/lib_$THE_CMS3_TAG.tar.gz
+cd $DIR
+echo "Your tarball is lib_$THE_CMS3_TAG.tar.gz"
