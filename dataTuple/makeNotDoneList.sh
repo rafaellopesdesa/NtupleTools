378ca4a88533cb93ca3daaae477895d6772510a8
Christopher A. George <george@physics.ucsb.edu>
2015-03-06 16:31:46 -0800

    change file names

--- dataTuple/makeNotDoneList.sh
+++ dataTuple/makeNotDoneList.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sort fakeMasterList.txt > temp.txt
-mv temp.txt fakeMasterList.txt
-sort fakeCompletedList.txt > temp.txt
-mv temp.txt fakeCompletedList.txt
-diff fakeMasterList.txt fakeCompletedList.txt > notDoneList.txt 
+sort masterList.txt > temp.txt
+mv temp.txt masterList.txt
+sort completedList.txt > temp.txt
+mv temp.txt completedList.txt
+diff masterList.txt completedList.txt > notDoneList.txt 
 sed -i 's/<\ //g' notDoneList.txt
 sed -i '1d' notDoneList.txt
