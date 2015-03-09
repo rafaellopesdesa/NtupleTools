f924afdcec98f88dc2004136d68d30e2b9096c4e
jgran <jgran@physics.ucsb.edu>
2015-03-09 16:59:34 -0700

    remove lines left over from diff

--- dataTuple/makeNotDoneList.sh
+++ dataTuple/makeNotDoneList.sh
@@ -6,4 +6,4 @@ sort /nfs-7/userdata/dataTuple/completedList.txt > temp_completedList.txt
 mv temp_completedList.txt /nfs-7/userdata/dataTuple/completedList.txt
 diff masterList.txt /nfs-7/userdata/dataTuple/completedList.txt > notDoneList.txt 
 sed -i 's/<\ //g' notDoneList.txt
-sed -i '1d' notDoneList.txt
+sed -i '/root/!d' notDoneList.txt
