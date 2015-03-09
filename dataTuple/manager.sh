93bca039c8bdeff3f671aeae6d8e641c07468f5f
jgran <jgran@physics.ucsb.edu>
2015-03-09 14:18:32 -0700

    update

--- dataTuple/manager.sh
+++ dataTuple/manager.sh
@@ -165,8 +165,6 @@ then
         $currentLine > failure.txt
         continue
       fi
-      else
-        echo "Too many jobs for this file have failed. Skipping $currentLine"
     fi
 
     #5b. Submit them
