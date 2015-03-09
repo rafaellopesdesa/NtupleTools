f09ab1234baf3f31e2c996aa8193493f46b8d896
jgran <jgran@physics.ucsb.edu>
2015-03-08 19:31:21 -0700

    change temp file name

--- dataTuple/makeNotDoneList.sh
+++ dataTuple/makeNotDoneList.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sort masterList.txt > temp.txt
-mv temp.txt masterList.txt
-sort completedList.txt > temp.txt
-mv temp.txt completedList.txt
+sort masterList.txt > temp_masterList.txt
+mv temp_masterList.txt masterList.txt
+sort completedList.txt > temp_completedList.txt
+mv temp_completedList.txt completedList.txt
 diff masterList.txt completedList.txt > notDoneList.txt 
 sed -i 's/<\ //g' notDoneList.txt
 sed -i '1d' notDoneList.txt
