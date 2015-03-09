54b8d7bc2956ec07c9938a095ef7ef7cadb8d6f1
jgran <jgran@physics.ucsb.edu>
2015-03-09 15:49:05 -0700

    use correct completedList

--- dataTuple/makeNotDoneList.sh
+++ dataTuple/makeNotDoneList.sh
@@ -2,8 +2,8 @@
 
 sort masterList.txt > temp_masterList.txt
 mv temp_masterList.txt masterList.txt
-sort completedList.txt > temp_completedList.txt
-mv temp_completedList.txt completedList.txt
-diff masterList.txt completedList.txt > notDoneList.txt 
+sort /nfs-7/userdata/dataTuple/completedList.txt > temp_completedList.txt
+mv temp_completedList.txt /nfs-7/userdata/dataTuple/completedList.txt
+diff masterList.txt /nfs-7/userdata/dataTuple/completedList.txt > notDoneList.txt 
 sed -i 's/<\ //g' notDoneList.txt
 sed -i '1d' notDoneList.txt
