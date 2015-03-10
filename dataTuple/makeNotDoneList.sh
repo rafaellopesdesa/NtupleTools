49a4fc4ad25e2fbd2a2ebce5314cb8b1fd9d335d
jgran <jgran@physics.ucsb.edu>
2015-03-10 09:59:39 -0700

    add comments

--- dataTuple/makeNotDoneList.sh
+++ dataTuple/makeNotDoneList.sh
@@ -5,5 +5,5 @@ mv temp_masterList.txt masterList.txt
 sort /nfs-7/userdata/dataTuple/completedList.txt > temp_completedList.txt
 mv temp_completedList.txt /nfs-7/userdata/dataTuple/completedList.txt
 diff masterList.txt /nfs-7/userdata/dataTuple/completedList.txt > notDoneList.txt 
-sed -i 's/<\ //g' notDoneList.txt
-sed -i '/root/!d' notDoneList.txt
+sed -i 's/<\ //g' notDoneList.txt #remove stuff left over from diff
+sed -i '/root/!d' notDoneList.txt #remove stuff left over from diff
