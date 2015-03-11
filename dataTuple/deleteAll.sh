2fbb043cb975d0ac23e2edfa7c0e81b2ed33f541
jgran <jgran@physics.ucsb.edu>
2015-03-11 15:20:19 -0700

    deleteAll.sh non-user specific

--- dataTuple/deleteAll.sh
+++ dataTuple/deleteAll.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 crontab -r
-condor_rm cgeorge
+condor_rm $USER
 sleep 10
 rm /nfs-7/userdata/dataTuple/completedList.txt
-rm /hadoop/cms/store/user/cgeorge/condor/dataNtupling/dataTuple/*.root
-rm /home/users/cgeorge/NtupleTools/dataTuple/*.txt
-rm -rf /home/users/cgeorge/NtupleTools/dataTuple/cms3withCondor
-rm -rf /home/users/cgeorge/NtupleTools/dataTuple/cms3withCondor
+rm /hadoop/cms/store/user/$USER/condor/dataNtupling/dataTuple/*.root
+rm /home/users/$USER/NtupleTools/dataTuple/*.txt
+rm -rf /home/users/$USER/NtupleTools/dataTuple/cms3withCondor
+rm -rf /home/users/$USER/NtupleTools/dataTuple/cms3withCondor
