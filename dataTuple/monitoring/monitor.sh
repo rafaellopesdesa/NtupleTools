f66639d8ba28a9200abd0c797e224f7688637385
jgran <jgran@physics.ucsb.edu>
2015-03-11 13:58:09 -0700

    restructure monitoring

--- dataTuple/monitoring/monitor.sh
+++ dataTuple/monitoring/monitor.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-source getListOfDatasets.sh
-source getNumberOfFilesCompleted.sh
-source getNumberOfFilesTotal.sh
-source getNumberOfJobsRunning.sh
+OUT=/nfs-7/userdata/dataTuple/dataTupleMonitor.txt
+
+sed 's./. .g' /nfs-7/userdata/dataTuple/input.txt | awk '{print $1}' > listOfDatasets.txt #replace "/" by " " and then print out the first column
+
+echo "Last updated: `date`" > $OUT
+echo "" >> $OUT
+
+echo "List of datasets being processed:" >> $OUT
+cat listOfDatasets.txt >> $OUT
+
+echo "" >> $OUT
+
+while read line
+do
+  DATASET=$line
+  NTOTAL=`cat ../masterList.txt | grep $line | wc -l`
+  NCOMPLETED=`cat /nfs-7/userdata/dataTuple/completedList.txt | grep $line | wc -l`
+  NJOBS=`cat ../submitList.txt | grep $line | wc -l > numberOfJobsRunning_$line.txt`
+  echo "Dataset: $DATASET" >> $OUT
+  echo "Number of available files: $NTOTAL" >> $OUT
+  echo "Number of files processed: $NCOMPLETED" >> $OUT
+  echo "Number of jobs running: $NJOBS" >> $OUT
+  echo "" >> $OUT
+done < listOfDatasets.txt
+
+rm listOfDatasets.txt
