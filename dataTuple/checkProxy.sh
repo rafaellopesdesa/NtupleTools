d7cabd595b828fcd66804e71a863e680292b9cd8
Christopher A. George <george@physics.ucsb.edu>
2015-03-11 14:05:26 -0700

    fix

--- dataTuple/checkProxy.sh
+++ dataTuple/checkProxy.sh
@@ -19,7 +19,7 @@ lineWithTimeLeft=`echo $linesWithTimeLeft | awk '{print $NF}'`
 lineWithPath=`sed -n /path/= voms_status.txt`
 hoursLeftOnProxy=`awk -v var="$lineWithTimeLeft" 'NR==var {print $3}' voms_status.txt | tr ':' ' ' | awk '{print $1}'`
 pathToProxy=`awk -v var="$lineWithPath" 'NR==var {print $3}' voms_status.txt`
-if [ `echo $(( ($hoursLeftOnProxy) < "4"))` == 1 ]
+if [ `echo $(( ($hoursLeftOnProxy) < 4))` == 1 ]
 then
   voms-proxy-init -q -voms cms -hours 120 -valid=120:0 -cert=$pathToProxy
 fi
