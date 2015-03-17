f05d26d7dff617c49fe6218dc595fcc7e5b38d9a
Christopher A. George <george@physics.ucsb.edu>
2015-03-17 12:50:53 -0700

    various updates

--- dataTuple/GenerateMasterList.sh
+++ dataTuple/GenerateMasterList.sh
@@ -5,5 +5,14 @@
 readarray -t samples < /nfs-7/userdata/dataTuple/input.txt
 for i in "${samples[@]}"
 do
-  ./das_client.py --query="file dataset= $i" | grep "^/store" >> masterList.txt
+  input_from_das=`./das_client.py --query="file dataset= $i"` 
+  echo "$input_from_das" | grep "status: fail"
+  if [ $? == 0 ]
+  then 
+    echo "failed" >> nQueryAttempts.txt
+    if [ -e nQueryAttemts.txt ] && [ "$(cat nQueryAttempts.txt | wc -l)" -gt "60" ]; then echo "DataTupleWarning! Query attempt has failed many times!" | /bin/mail -r "george@physics.ucsb.edu" -s "[dataTuple] error report" "george@physics.ucsb.edu, jgran@physics.ucsb.edu"; fi
+  else
+    rm nQueryAttempts.txt
+  fi
+  echo "$input_from_das" | grep "^/store" >> masterList.txt
 done
