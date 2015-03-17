8b8a6434caf32a17abd3a465dc5da9aed4cc1ade
Christopher A. George <george@physics.ucsb.edu>
2015-03-17 13:34:38 -0700

    bug fix

--- dataTuple/GenerateMasterList.sh
+++ dataTuple/GenerateMasterList.sh
@@ -12,7 +12,7 @@ do
     echo "failed" >> nQueryAttempts.txt
     if [ -e nQueryAttemts.txt ] && [ "$(cat nQueryAttempts.txt | wc -l)" -gt "60" ]; then echo "DataTupleWarning! Query attempt has failed many times!" | /bin/mail -r "george@physics.ucsb.edu" -s "[dataTuple] error report" "george@physics.ucsb.edu, jgran@physics.ucsb.edu"; fi
   else
-    rm nQueryAttempts.txt
+    rm nQueryAttempts.txt &>/dev/null
   fi
   echo "$input_from_das" | grep "^/store" >> masterList.txt
 done
--- dataTuple/dataTupleLauncher.sh
+++ dataTuple/dataTupleLauncher.sh
@@ -9,6 +9,16 @@ then
   rm runningList.txt
 fi
 
+if [ -e nQueryAttempts.txt ] 
+then
+  rm nQueryAttempts.txt &>/dev/null
+fi
+
+if [ -e emailAboutProxy.txt ] 
+then 
+  rm emailAboutProxy.txt
+fi 
+
 #Check Proxy
 voms-proxy-info --all &> voms_status.txt
 if grep "Couldn't find a valid proxy." voms_status.txt &>/dev/null
@@ -20,6 +30,6 @@ fi
 #Submit
 crontab -l > mycron 2>/dev/null
 #echo "* * * * * cd ${PWD} && /bin/sh ${PWD}/manager.sh >> ${PWD}/cron_log.log" >> mycron
-echo "* * * * * cd ${PWD} && /bin/sh ${PWD}/manager.sh >> /nfs-7/userdata/dataTuple/dataTuple.log" >> mycron
+echo "* * * * * cd ${PWD} && timeout 3600 /bin/sh ${PWD}/manager.sh >> /nfs-7/userdata/dataTuple/dataTuple.log" >> mycron && if [ $? == 137 ]; then cp /nfs-7/userdata/dataTuple/dataTuple.log /home/users/$USER/public_html/error_log.log; echo "Warning.  Job was killed after 60 mins.  See log in uaf-7.t2.ucsd.edu/~cgeorge/error_log.log" | /bin/mail -r "george@physics.ucsb.edu" -s "[dataTuple] error report" "george@physics.ucsb.edu, jgran@physics.ucsb.edu"; fi 
 crontab mycron
 rm mycron
