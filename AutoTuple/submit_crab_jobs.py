5b83405ac8e8936487e50fa8ec1eb01486dd1680
Christopher A. George <george@physics.ucsb.edu>
2015-04-04 14:54:05 -0700

    bug fix

--- AutoTuple/submit_crab_jobs.py
+++ AutoTuple/submit_crab_jobs.py
@@ -42,10 +42,11 @@ while (lnum <= inFile_size):
   line = linecache.getline(inFile, lnum)
   parts = line.split()
   lumi_line = linecache.getline('LumisPerJob.txt', lnum-2)
+  lnum+=1
   lumi_parts = lumi_line.split()
   numLumiPerJob = lumi_parts[0]
   if ((len(glob.glob("/hadoop/cms/store/user/" + getpass.getuser() + "/" + parts[0].split('/')[1] + "/crab_" + parts[0].split('/')[1] + "_" + parts[0].split('/')[2] + "/*/0000/*.root")) > 0) and redoCrab == 2):
-    redoCrab = int(input("Some unmerged files already exist!  Remake them? (1/0)"))
+    redoCrab = int(input("Some unmerged files already exist!  Remake them? (1/0) "))
   if (redoCrab == 0): continue
   print numLumiPerJob
   command = 'python makeCrab3Files.py -CMS3cfg skeleton_cfg.py -d ' + parts[0] + ' -t ' + tag + ' -gtag ' + gtag + ' -isData ' + parts[3] + ' -lumisPerJob ' + numLumiPerJob
@@ -56,4 +57,3 @@ while (lnum <= inFile_size):
   crab_dir = parts[0].split('/')[1]+'_'+parts[0].split('/')[2]
   os.system('crab submit -c cfg/' + crab_dir + '.py')
   print crab_dir
-  lnum+=1
