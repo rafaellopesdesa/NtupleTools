e89eebb7258445deb633e52f97749f4c2dbe59f3
Christopher A. George <george@physics.ucsb.edu>
2015-04-16 16:00:39 -0700

    improved post-processing stuff

--- cms3withCondor/condor_process.sh
+++ cms3withCondor/condor_process.sh
@@ -11,6 +11,15 @@ SPARM_CLASS=GLUINO
 SPARM_MASS=1200
 
 #####----- HERE THERE BE DRAGONS --------#####
+
+#Get condorMergingTools stuff
+cp ../condorMergingTools/makeListsForMergingCrab3.py .
+cp -r ../condorMergingTools/libC . 
+cp -r ../condorMergingTools/libsh . 
+cp -r ../condorMergingTools/submitMergeJobs.sh .
+if [ ! -d cfg ]; then mkdir cfg; fi
+
+#Get cross-section
 if [ $SPARM_CLASS == "GLUINO" ] 
 then
   temp2=`root -l -b -q go_xsec.C\($SPARM_MASS\)` 
@@ -21,7 +30,10 @@ else
   echo "ERROR!  Only gluinos and stops are supported!" 
   return;
 fi
-
 xsec=`echo $temp2 | awk '{ print $NF }' | cut -c 1-7 --complement`
 
+#Make lists
 python makeListsForMergingCrab3.py -d $dir -o $dir/merged/ -s $name -k 1 -e 1 -x $xsec --overrideCrab 
+
+#Submit it
+. submitMergeJobs.sh cfg/${name}_cfg.sh
