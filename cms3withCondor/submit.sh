ae72b484eec1d3a828fff69b3c7b491b16bd795b
Christopher A. George <george@physics.ucsb.edu>
2015-05-13 17:44:49 -0700

    copy tarball after you make it

--- cms3withCondor/submit.sh
+++ cms3withCondor/submit.sh
@@ -25,6 +25,7 @@ else
   if [ -e "libCMS3=lib_${CMS3_TAG}.tar.gz" ]
   then
     libCMS3=lib_${CMS3_TAG}.tar.gz
+    cp lib_${CMS3_TAG}.tar.gz /nfs-7/userdata/libCMS3/
   else
     echo "Failed to make libCMS3 tarball on the fly!"
     exit 1
