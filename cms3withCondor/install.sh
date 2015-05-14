8f125e48af9d2e0d208c8fd895dc0c7312dd2c3e
Christopher A. George <george@physics.ucsb.edu>
2015-05-13 17:38:16 -0700

    add missing install script

---
+++ cms3withCondor/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#USER INPUTS
+CMS3Tag=CMS3_V07-02-08
+CMSSW_release=7_2_0
+export SCRAM_ARCH=slc6_amd64_gcc481
+
+#--Here there be dragons----
+export CMS_PATH=/cvmfs/cms.cern.ch
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+scramv1 p -n CMSSW_$CMSSW_release CMSSW CMSSW_$CMSSW_release
+cd CMSSW_$CMSSW_release/src
+eval `scramv1 runtime -sh`
+git clone git@github.com:cmstas/NtupleMaker.git CMS3/NtupleMaker
+cd CMS3/NtupleMaker
+git checkout $CMS3Tag
+source setup/patchesToSource.sh
+cd $CMSSW_BASE/src
+scram b -j 10
+cd ..
