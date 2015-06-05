5659de842b856e5bc79bbeee6f066ce887ca7d5f
Christopher A. George <george@physics.ucsb.edu>
2015-06-04 19:06:55 -0700

    move validation stuff to NtupleTools

---
+++ validation/comparison/do.C
@@ -0,0 +1,10 @@
+{
+  gROOT->ProcessLine(".L compareNtuples.C+");
+  gROOT->ProcessLine("compareNtuples( \
+    \"/home/users/cgeorge/CMS3/CMSSW_7_4_1_patch1_oldTag/src/CMS3/NtupleMaker/ntuple.root\",  \
+    \"/home/users/cgeorge/CMS3/CMSSW_7_4_1_patch1/src/CMS3/NtupleMaker/ntuple.root\",  \
+    true, false)");
+
+}
+
+//    \"/hadoop/cms/store/group/snt/phys14/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola_Phys14DR-PU20bx25_PHYS14_25_V1-v1/V07-02-08/merged_ntuple_2.root\", \
