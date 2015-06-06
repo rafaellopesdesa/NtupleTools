fb88a342bc724bde0a71d65ba16355ac1966577e
Alex George <george@physics.ucsb.edu>
2015-06-06 15:18:43 -0700

    Update process.sh

--- validation/comparison/process.sh
+++ validation/comparison/process.sh
@@ -3,4 +3,4 @@
 sed -i 's/_/\\_/g' overview.tex 
 pdflatex overview.tex
 ps2pdf diff.ps
-gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -sOutputFile=results_full.pdf overview.pdf diff.pdf
+gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -sOutputFile=results.pdf overview.pdf diff.pdf
