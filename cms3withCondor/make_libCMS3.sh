2a748c35c2433de240fafdb1e4c65eee4f9ab6e3
Christopher A. George <george@physics.ucsb.edu>
2015-06-10 10:51:42 -0700

    fix

--- cms3withCondor/make_libCMS3.sh
+++ cms3withCondor/make_libCMS3.sh
@@ -16,12 +16,12 @@ sed -i "s/CMS3Tag=master/CMS3Tag=${THE_CMS3_TAG}/g" install.sh
 
 DIR=$PWD
 
-stuff=`find src/ -name "data"`
-stuff2=`find src/ -name "interface"`
 source install.sh
 cd $CMSSW_BASE
 echo "Making the tarball..."
-tar -chzvf lib_$THE_CMS3_TAG.tar.gz lib/ python/ $stuff $stuff2 src/CMS3/NtupleMaker/test/*_cfg.py
+stuff1=`find src/ -name "data"`
+stuff2=`find src/ -name "interface"`
+tar -chzvf lib_$THE_CMS3_TAG.tar.gz lib/ python/ $stuff1 $stuff2 src/CMS3/NtupleMaker/test/*_cfg.py
 
 
 mv lib_$THE_CMS3_TAG.tar.gz $DIR/lib_$THE_CMS3_TAG.tar.gz
