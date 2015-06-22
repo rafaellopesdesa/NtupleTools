a30e93e4c4cd3698951e7a9d28dcae6d0f953920
Christopher A. George <george@physics.ucsb.edu>
2015-06-22 15:43:45 -0700

    blah

--- dataTuple/checkFile.sh
+++ dataTuple/checkFile.sh
@@ -37,30 +37,30 @@ rm validFileOutput.txt
 if [ "$isGood" == "1" ] 
 then
   taskName=`echo $2 | tr '/' ' ' |  awk '{print $3"_"$4"_"$5"_"$6}'`
-  mergedFileNumber=`less /nfs-7/userdata/dataTuple/mergedLists/$taskName/status.txt | head -1 | awk '{print $3}'`
-  echo $2 >> /nfs-7/userdata/dataTuple/mergedLists/$taskName/merged_list_$mergedFileNumber.txt 
-  currentSize=`less /nfs-7/userdata/dataTuple/mergedLists/$taskName/status.txt | head -2 | tail -1 | awk '{print $3}'`
-  currentNumber=`less /nfs-7/userdata/dataTuple/mergedLists/$taskName/status.txt | head -3 | tail -1 | awk '{print $3}'`
+  mergedFileNumber=`less $BASEPATH/mergedLists/$taskName/status.txt | head -1 | awk '{print $3}'`
+  echo $2 >> $BASEPATH/mergedLists/$taskName/merged_list_$mergedFileNumber.txt 
+  currentSize=`less $BASEPATH/mergedLists/$taskName/status.txt | head -2 | tail -1 | awk '{print $3}'`
+  currentNumber=`less $BASEPATH/mergedLists/$taskName/status.txt | head -3 | tail -1 | awk '{print $3}'`
   newFileSize=`ls -l $1 | awk '{print $5}'`
   newTotalSize=$(( $newFileSize + $currentSize ))
   newFileNevents=`rot getNevents.C\(\"$1\"\) &> blah.txt; less blah.txt | tail -1 | cut -c 1-5 --complement ; rm blah.txt`
   newTotalNevents=$(( $currentNumber + $newFileNevents ))
-  sed -i "s/current\ size\ (bytes).*/current size (bytes): $newTotalSize/" /nfs-7/userdata/dataTuple/mergedLists/$taskName/status.txt
-  sed -i "s/current\ nEvents.*/current nEvents: $newTotalNevents/" /nfs-7/userdata/dataTuple/mergedLists/$taskName/status.txt
+  sed -i "s/current\ size\ (bytes).*/current size (bytes): $newTotalSize/" $BASEPATH/mergedLists/$taskName/status.txt
+  sed -i "s/current\ nEvents.*/current nEvents: $newTotalNevents/" $BASEPATH/mergedLists/$taskName/status.txt
 
   #If we now have enough to run post-processing, prepare the files 
-  if [ "$newTotalSize" -gt "4000000000" ]
+  if [ "$newTotalSize" -gt "300000000" ]
   then
     #(a) Reset counters for this script
-    sed -i "s/currently filling.*/currently filling: $(( $mergedFileNmuber + 1 ))/" /nfs-7/userdata/dataTuple/mergedLists/$taskName/status.txt
-    sed -i "s/current\ size\ (bytes).*/current size (bytes): 0/" /nfs-7/userdata/dataTuple/mergedLists/$taskName/status.txt
-    sed -i "s/current\ nEvents.*/current nEvents: 0/" /nfs-7/userdata/dataTuple/mergedLists/$taskName/status.txt
+    sed -i "s/currently filling.*/currently filling: $(( $mergedFileNmuber + 1 ))/" $BASEPATH/mergedLists/$taskName/status.txt
+    sed -i "s/current\ size\ (bytes).*/current size (bytes): 0/" $BASEPATH/mergedLists/$taskName/status.txt
+    sed -i "s/current\ nEvents.*/current nEvents: 0/" $BASEPATH/mergedLists/$taskName/status.txt
     #(b) Write the meta data file
-    echo "n: $newTotalNevents" >> /nfs-7/userdata/dataTuple/mergedLists/$taskName/metaData_$merged_file_number.txt
-    echo "k: 1" >> /nfs-7/userdata/dataTuple/mergedLists/$taskName/metaData_$merged_file_number.txt
-    echo "f: 1" >> /nfs-7/userdata/dataTuple/mergedLists/$taskName/metaData_$merged_file_number.txt
-    echo "x: 1" >> /nfs-7/userdata/dataTuple/mergedLists/$taskName/metaData_$merged_file_number.txt
-    echo "file: merged_list_$merged_file_number.txt" >> /nfs-7/userdata/dataTuple/mergedLists/$taskName/metaData_$merged_file_number.txt
+    echo "n: $newTotalNevents" >> $BASEPATH/mergedLists/$taskName/metaData_$merged_file_number.txt
+    echo "k: 1" >> $BASEPATH/mergedLists/$taskName/metaData_$merged_file_number.txt
+    echo "f: 1" >> $BASEPATH/mergedLists/$taskName/metaData_$merged_file_number.txt
+    echo "x: 1" >> $BASEPATH/mergedLists/$taskName/metaData_$merged_file_number.txt
+    echo "file: merged_list_$merged_file_number.txt" >> $BASEPATH/mergedLists/$taskName/metaData_$merged_file_number.txt
     #(c) Submit it
     . submitPPJob.sh $taskName $merged_file_number
   fi
