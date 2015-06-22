31efd90b45461108c381624d8720c323e146c89c
Alex George <george@physics.ucsb.edu>
2015-06-22 13:27:12 -0700

    Update submitPPJob.sh

--- dataTuple/submitPPJob.sh
+++ dataTuple/submitPPJob.sh
@@ -1,15 +1,23 @@
 #This takes two arguments
   #1) the directory in /nfs-7/userdata/dataTuple/mergedLists/
   #2) the number file you want to make or remake
+  #3) the basepath.  For CMS3 resubmissions, this is /nfs-7/userdata/dataTuple
 
 #You must run this script from inside the dataTuple directory!  
 
 sample=$1
 number=$2
 
+if [ "$#" -gt "2" ]
+then
+  BASEPATH=$3
+else
+  BASEPATH="$PWD/$BASEPATH"
+fi
+
 #Set the environment variables
-export inputListDirectory=/nfs-7/userdata/dataTuple/mergedLists/$sample/merged_list_$number.txt
-export mData=/nfs-7/userdata/dataTuple/mergedLists/$sample/metaData_$number.txt
+export inputListDirectory=$BASEPATH/mergedLists/$sample/merged_list_$number.txt
+export mData=$BASEPATH/mergedLists/$sample/metaData_$number.txt
 export outputDir=/hadoop/cms/store/user/$USER/condor/dataNtupling/merged/$sample
 export dataSet=$sample
 export workingDirectory=$PWD
