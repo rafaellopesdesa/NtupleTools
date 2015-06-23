bf079db9fdc918227f42da2fccfafd5c9388c9be
Christopher A. George <george@physics.ucsb.edu>
2015-06-22 17:05:10 -0700

    fix fucking error

--- dataTuple/nJobsRunning.sh
+++ dataTuple/nJobsRunning.sh
@@ -1,16 +1,9 @@
 #!/bin/bash 
 
 condor_q $USER -const 'isDataTupleCMS3flagged==true' > jobsRunning.txt
-nJobsRunning=`awk 'END{print $1}' jobsRunning.txt`
-if [ "$nJobsRunning" == "ID" ] 
-then
-  return 1
-fi
-
-nJobsRunning=`echo $nJobsRunning | rev | cut -c 1-2 --complement | rev`
+nJobsRunning=`less jobsRunning.txt | wc -l`
 
 if [ "$nJobsRunning" -gt "2000" ]
 then
   echo "Data Tuple Warning!  You are running more than 2000 jobs." | /bin/mail -r "george@physics.ucsb.edu" -s "[dataTuple] warning" "george@physics.ucsb.edu, jgran@physics.ucsb.edu" 
 fi
-        
