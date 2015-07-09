5856a58f09c99941b8f16c825f45ddecdb020297
Jason Gran <jgran@physics.ucsb.edu>
2015-07-09 12:12:24 -0700

    change to run on all files

--- dataTuple/GenerateMasterList.sh
+++ dataTuple/GenerateMasterList.sh
@@ -10,7 +10,7 @@ fi
 readarray -t samples < $BASEPATH/input.txt
 for i in "${samples[@]}"
 do
-  input_from_das=`./das_client.py --query="file dataset= $i site=T2_US_*" --limit=10` #need to use --limit=0 to pick up all files!
+  input_from_das=`./das_client.py --query="file dataset= $i site=T2_US_*" --limit=0` #need to use --limit=0 to pick up all files!
   echo "$input_from_das" | grep "status: fail"
   if [ $? == 0 ]
   then 
