4a6677094c685d6d147966bf3e9d35a559179f2f
Christopher A. George <george@physics.ucsb.edu>
2015-07-09 22:44:37 -0700

    explictely check basepath

--- dataTuple/checkFile.sh
+++ dataTuple/checkFile.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+BASEPATH=$1
 sampleName=$3
 JOBTYPE=$4
 
@@ -10,7 +11,9 @@ fi
 
 if [ "$JOBTYPE" == "cms3" ]
 then
+  echo "./sweepRoot -o Events -t Events $2 > validFileOutput.txt"
   ./sweepRoot -o Events -t Events $2 > validFileOutput.txt
+  echo "done" 
 elif [ "$JOBTYPE" == "user" ]
 then
   ./sweepRoot -o Events $2 > validFileOutput.txt
@@ -48,53 +51,53 @@ done
 
 rm validFileOutput.txt
 
-currentNumber=0
-currentSize=0
-newFileNevents=0
-
-#If the file is good, add it to list for post-processing
-if [ "$isGood" == "1" ] 
-then
-  taskName=`echo $3 | tr '/' ' ' |  awk '{print $3"_"$4"_"$5"_"$6}'`
-  if [ ! -d $BASEPATH/mergedLists/$taskName ]; then mkdir -p $BASEPATH/mergedLists/$taskName; fi
-  if [ ! -e $BASEPATH/mergedLists/$taskName/status.txt ]
-  then
-    echo "currently filling: 0" >> $BASEPATH/mergedLists/$taskName/status.txt
-    echo "current size (bytes): 0" >> $BASEPATH/mergedLists/$taskName/status.txt
-    echo "current nEvents: 0" >> $BASEPATH/mergedLists/$taskName/status.txt
-  fi
-  mergedFileNumber=`less $BASEPATH/mergedLists/$taskName/status.txt | head -1 | awk '{print $3}'`
-  echo $2 >> $BASEPATH/mergedLists/$taskName/merged_list_$mergedFileNumber.txt 
-  currentSize=`less $BASEPATH/mergedLists/$taskName/status.txt | head -2 | tail -1 | awk '{print $4}'`
-  currentNumber=`less $BASEPATH/mergedLists/$taskName/status.txt | head -3 | tail -1 | awk '{print $3}'`
-  newFileSize=`ls -l $2 | awk '{print $5}'`
-  newTotalSize=$(( $newFileSize + $currentSize ))
-  newFileNevents=`root -b -q getNevents.C\(\"$2\"\) &> blah.txt; less blah.txt | tail -1 | cut -c 1-5 --complement ; rm blah.txt`
-  newTotalNevents=$(( $currentNumber + $newFileNevents ))
-  sed -i "s/current\ size\ (bytes).*/current size (bytes): $newTotalSize/" $BASEPATH/mergedLists/$taskName/status.txt
-  sed -i "s/current\ nEvents.*/current nEvents: $newTotalNevents/" $BASEPATH/mergedLists/$taskName/status.txt
-
-  #If we now have enough to run post-processing, prepare the files 
-  if [ "$newTotalSize" -gt "4000000000" ]
-  then
-    #(a) Reset counters for this script
-    sed -i "s/currently filling.*/currently filling: $(( $mergedFileNumber + 1 ))/" $BASEPATH/mergedLists/$taskName/status.txt
-    sed -i "s/current\ size\ (bytes).*/current size (bytes): 0/" $BASEPATH/mergedLists/$taskName/status.txt
-    sed -i "s/current\ nEvents.*/current nEvents: 0/" $BASEPATH/mergedLists/$taskName/status.txt
-    #(b) Write the meta data file
-    echo "n: $newTotalNevents" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
-    echo "k: 1" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
-    echo "f: 1" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
-    echo "x: 1" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
-    echo "file: merged_list_$mergedFileNumber.txt" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
-    #(c) Submit it
-    echo "submit: submitPPJob.sh $taskName $mergedFileNumber $JOBTYPE" 
-    . submitPPJob.sh $taskName $mergedFileNumber $JOBTYPE
-    if [ ! -e submitPPList.txt ]; then touch submitPPList.txt; fi
-    submitTime=`date +%s`
-    echo "/hadoop/cms/store/user/$USER/dataTuple/$taskName/merged/merged_ntuple_$mergeFileNumber.root $submitTime" >> submitPPList.txt
-  fi
-
-fi
-
-isGood=0
+#currentNumber=0
+#currentSize=0
+#newFileNevents=0
+#
+##If the file is good, add it to list for post-processing
+#if [ "$isGood" == "1" ] 
+#then
+#  taskName=`echo $3 | tr '/' ' ' |  awk '{print $3"_"$4"_"$5"_"$6}'`
+#  if [ ! -d $BASEPATH/mergedLists/$taskName ]; then mkdir -p $BASEPATH/mergedLists/$taskName; fi
+#  if [ ! -e $BASEPATH/mergedLists/$taskName/status.txt ]
+#  then
+#    echo "currently filling: 0" >> $BASEPATH/mergedLists/$taskName/status.txt
+#    echo "current size (bytes): 0" >> $BASEPATH/mergedLists/$taskName/status.txt
+#    echo "current nEvents: 0" >> $BASEPATH/mergedLists/$taskName/status.txt
+#  fi
+#  mergedFileNumber=`less $BASEPATH/mergedLists/$taskName/status.txt | head -1 | awk '{print $3}'`
+#  echo $2 >> $BASEPATH/mergedLists/$taskName/merged_list_$mergedFileNumber.txt 
+#  currentSize=`less $BASEPATH/mergedLists/$taskName/status.txt | head -2 | tail -1 | awk '{print $4}'`
+#  currentNumber=`less $BASEPATH/mergedLists/$taskName/status.txt | head -3 | tail -1 | awk '{print $3}'`
+#  newFileSize=`ls -l $2 | awk '{print $5}'`
+#  newTotalSize=$(( $newFileSize + $currentSize ))
+#  newFileNevents=`root -b -q getNevents.C\(\"$2\"\) &> blah.txt; less blah.txt | tail -1 | cut -c 1-5 --complement ; rm blah.txt`
+#  newTotalNevents=$(( $currentNumber + $newFileNevents ))
+#  sed -i "s/current\ size\ (bytes).*/current size (bytes): $newTotalSize/" $BASEPATH/mergedLists/$taskName/status.txt
+#  sed -i "s/current\ nEvents.*/current nEvents: $newTotalNevents/" $BASEPATH/mergedLists/$taskName/status.txt
+#
+#  #If we now have enough to run post-processing, prepare the files 
+#  if [ "$newTotalSize" -gt "4000000000" ]
+#  then
+#    #(a) Reset counters for this script
+#    sed -i "s/currently filling.*/currently filling: $(( $mergedFileNumber + 1 ))/" $BASEPATH/mergedLists/$taskName/status.txt
+#    sed -i "s/current\ size\ (bytes).*/current size (bytes): 0/" $BASEPATH/mergedLists/$taskName/status.txt
+#    sed -i "s/current\ nEvents.*/current nEvents: 0/" $BASEPATH/mergedLists/$taskName/status.txt
+#    #(b) Write the meta data file
+#    echo "n: $newTotalNevents" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
+#    echo "k: 1" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
+#    echo "f: 1" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
+#    echo "x: 1" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
+#    echo "file: merged_list_$mergedFileNumber.txt" >> $BASEPATH/mergedLists/$taskName/metaData_$mergedFileNumber.txt
+#    #(c) Submit it
+#    echo "submit: submitPPJob.sh $taskName $mergedFileNumber $JOBTYPE" 
+#    . submitPPJob.sh $taskName $mergedFileNumber $JOBTYPE
+#    if [ ! -e submitPPList.txt ]; then touch submitPPList.txt; fi
+#    submitTime=`date +%s`
+#    echo "/hadoop/cms/store/user/$USER/dataTuple/$taskName/merged/merged_ntuple_$mergeFileNumber.root $submitTime" >> submitPPList.txt
+#  fi
+#
+#fi
+#
+#isGood=0
