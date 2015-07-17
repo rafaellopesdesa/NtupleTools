93c3eae7e1b0d278afaf96d32a4ed7c24ed72d6f
Christopher A. George <george@physics.ucsb.edu>
2015-07-16 23:03:17 -0700

    fix proxy renewal problem

--- dataTuple/checkProxy.sh
+++ dataTuple/checkProxy.sh
@@ -19,7 +19,7 @@ linesWithTimeLeft=`sed -n /timeleft/= voms_status.txt`
 lineWithTimeLeft=`echo $linesWithTimeLeft | awk '{print $NF}'`
 lineWithPath=`sed -n /path/= voms_status.txt`
 hoursLeftOnProxy=`awk -v var="$lineWithTimeLeft" 'NR==var {print $3}' voms_status.txt | tr ':' ' ' | awk '{print $1}'`
-pathToProxy=`awk -v var="$lineWithPath" 'NR==var {print $3}' voms_status.txt`
+pathToProxy="/home/users/$USER/.globus/proxy_for_$USER.file"
 if [ `echo $(( ($hoursLeftOnProxy) < 4))` == 1 ]
 then
   voms-proxy-init -q -voms cms -hours 120 -valid=120:0 -cert=$pathToProxy
