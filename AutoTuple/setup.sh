40749d58c1babeab4c79e76e0121cbf475a07195
Alex George <george@physics.ucsb.edu>
2015-08-07 13:26:27 -0700

    set env before making sweepRoot to avoid problems

--- AutoTuple/setup.sh
+++ AutoTuple/setup.sh
@@ -13,8 +13,10 @@ export PATH=$PATH:`pwd`
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 export SCRAM_ARCH=slc6_amd64_gcc491
 scramv1 p -n $CMSSW_VER CMSSW $CMSSW_VER
-pushd .
-cd ../sweepRoot
+pushd $CMSSW_VER
+cmsenv
+popd
+pushd ../sweepRoot
 make
 if [ ! -e sweepRoot.o ]
 then
