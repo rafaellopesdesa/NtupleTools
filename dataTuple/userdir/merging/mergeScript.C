c7c6209921c61fd065663824cb84dfcaa3bcbef9
Nick Amin <amin.nj@gmail.com>
2015-08-27 23:37:56 -0700

    for datatupler

--- dataTuple/userdir/merging/mergeScript.C
+++ dataTuple/userdir/merging/mergeScript.C
@@ -37,8 +37,8 @@ int mergeScript(const string& fileList, const string& outFile ){
 	file>>ntupleLocation;
 	if (ntupleLocation.empty()) continue;
 	else{
-      cout << ("root://cmsxrootd.fnal.gov///" + ntupleLocation.substr(11)).c_str() << endl;
-      ch1.Add( ("root://cmsxrootd.fnal.gov///" + ntupleLocation.substr(11)).c_str() );
+      cout << (ntupleLocation.substr(11)).c_str() << endl;
+      ch1.Add( (ntupleLocation.substr(11)).c_str() );
 	}
   }
 
