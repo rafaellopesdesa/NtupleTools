27e7a565006599c1c615ce28bf517311161b278a
jgran <jgran@physics.ucsb.edu>
2015-09-21 19:17:45 -0700

    disable nEvents check against DAS for now

--- dataTuple/checkFile.sh
+++ dataTuple/checkFile.sh
@@ -35,7 +35,7 @@ lineNo_forNEvents=`sed -n /$filename_escaped/= numEventsList.txt`
 goodNevents=`awk -v var="$lineNo_forNEvents" 'NR==var {print $2}' numEventsList.txt`
 ourNeventsTemp=$( root -b -q getNevents.C\(\"$2\"\) )
 ourNevents=`echo "$ourNeventsTemp" | tail -1 | awk '{print $NF}'`
-if [ "$goodNevents" != "$ourNevents" ]; then echo "FAILING! $ourNevents is not right, should be $goodNevents"; isGood=0; fi;
+#if [ "$goodNevents" != "$ourNevents" ]; then echo "FAILING! $ourNevents is not right, should be $goodNevents"; isGood=0; fi;//disable for now (DAS sucks)
 
 readarray -t results < validFileOutput.txt
 for i in "${results[@]}"
