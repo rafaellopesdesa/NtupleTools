8e91fcb10725d2a7ede234a5bed00379ec8f5fa3
Christopher A. George <george@physics.ucsb.edu>
2015-09-27 01:16:18 -0700

    GRRRRRRRRR

--- AutoTuple/setup.sh
+++ AutoTuple/setup.sh
@@ -43,7 +43,7 @@ mkdir autoTupleLogs
 cp ../../../sweepRoot/sweepRoot ${CMSSW_BASE}/crab/
 cp -r ../../../condorMergingTools/* ${CMSSW_BASE}/crab/
 cp ${CMSSW_BASE}/src/CMS3/NtupleMaker/test/MCProduction2015_NoFilter_cfg.py skeleton_cfg.py
-cp ${CMSSW_BASE}/src/CMS3/NtupleMaker/test/MCProduction2015_NoFilter_cfg.py skeleton_fsim_cfg.py
+cp ${CMSSW_BASE}/src/CMS3/NtupleMaker/test/MCProduction2015_FastSim_NoFilter_cfg.py skeleton_fsim_cfg.py
 sed -i s/process.GlobalTag.globaltag\ =\ .*/process.GlobalTag.globaltag\ =\ \"$gtag\"/ skeleton_cfg.py
 sed -i s/process.GlobalTag.globaltag\ =\ .*/process.GlobalTag.globaltag\ =\ \"$gtag\"/ skeleton_fsim_cfg.py
 cp ../../submitMergeJobs.sh .
