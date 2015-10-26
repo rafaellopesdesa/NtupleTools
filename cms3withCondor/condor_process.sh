5cd837fc4a4b7de118807a6db7f2be57bc1883b7
Christopher A. George <george@physics.ucsb.edu>
2015-10-26 11:19:02 -0700

    add BR

--- cms3withCondor/condor_process.sh
+++ cms3withCondor/condor_process.sh
@@ -2,13 +2,15 @@
 
 #User input
 #Directory where the babies are stored
-dir=/hadoop/cms/store/user/cgeorge/condor/SSPrivate/v07-04-08/
+dir=/hadoop/cms/store/user/cgeorge/condor/SSPrivate/v07-04-08/$1
 #Nice name for sample
 name=$1
 #Sparm class -- GLUINO, STOP, or OTHER
 SPARM_CLASS=$2
 #Sparm mass
 SPARM_MASS=$3
+#Branching Ratio (if not 1)
+if [ $# == 4 ] ; then BR=$4 ; else BR=1.0 ; fi
 
 #####----- HERE THERE BE DRAGONS --------#####
 
@@ -30,10 +32,12 @@ else
   echo "ERROR!  Only gluinos and stops are supported, not $SPARM_CLASS!" 
   return;
 fi
-xsec=`echo $temp2 | awk '{ print $NF }' | cut -c 1-7 --complement`
+xsec_temp=`echo $temp2 | awk '{ print $NF }' | sed -e 's/[eE]+*/\\*10\\^/'` 
+xsec=`echo "scale=9; $xsec_temp*$BR" | bc`
+echo $xsec
 
 #Make lists
-python makeListsForMergingCrab3.py -d $dir/$name -o $dir/$name/merged/ -s $name -k 1 -e 1 -x $xsec --overrideCrab 
+python makeListsForMergingCrab3.py -d $dir -o $dir/$name/merged/ -s $name -k 1 -e 1 -x $xsec --overrideCrab 
 
 #Submit it
 . submitMergeJobs.sh cfg/${name}_cfg.sh
